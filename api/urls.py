1f3744154664bbf5af0adaad725c5790e6319f17
Harsha Arora <harshamadaan50@gmail.com>
2026-06-27 23:45:48 +0530

    Initial commit

---
+++ api/urls.py
@@ -0,0 +1,12 @@
+from django.urls import path
+from .views import *
+
+urlpatterns = [
+    path('api/', getRoutes, name="routes"),
+    path('notes/', getNotes, name="notes"),
+    path('notes/<str:pk>/update/', updateNote, name="update-note"),
+    path('notes/<str:pk>/delete/', deleteNote, name="delete-note"),
+    path('notes/create/', createNote, name="create-note"),
+    path('notes/<str:pk>/', getNote, name="note"),
+    path('', notes),
+]
